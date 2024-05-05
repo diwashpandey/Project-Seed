d75be1101c68ec72907c1b4fe4d5095d1b489941
Diwash Pandey <155344419+diwashpandey@users.noreply.github.com>
2024-05-05 12:15:59 +0545

    Built Basic Frontend
    Set up the basic frontend using React and Tailwind. All frontend code is organized in a folder named "frontend".
    Utilized Vite for project setup.
    Added necessary dependencies to the package.json file. More will be added as the project progresses.

---
+++ frontend/ProjectSeed/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+function App() {
+
+  return (
+    <body className="bg-gray-800 h-screen w-screen flex justify-center items-center">
+     <h1 className="text-9xl text-white select-none">Project Seed</h1>
+    </body>
+  )
+}
+
+export default App
