e4d8a9dae41fc0784cdfbd6eac65a91927aa7edf
Diwash Pandey <155344419+diwashpandey@users.noreply.github.com>
2024-05-05 12:59:43 +0545

    Added all Apps that are required.
    Every required apps has been added along with:
    - urls.py
    - serializers.py
    
    Added apps:
    1. Accounts
    2. Home
    3. Posts
    4. Colleges
    5. Universities
    6. Ranking
    7. Recommendation

--- backend/ProjectSeed/ProjectSeed/settings.py
+++ backend/ProjectSeed/ProjectSeed/settings.py
@@ -37,6 +37,18 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
+
+
+    # Custom made APPS
+    'accounts',
+    'home',
+    'posts',
+    'colleges',
+    'universities',
+    'ranking',
+    'recommendation',
 ]
 
 MIDDLEWARE = [
