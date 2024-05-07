a7c39326eb9cba55a40de22f34b780e7966ed84a
Diwash Pandey <155344419+diwashpandey@users.noreply.github.com>
2024-05-07 07:16:47 +0545

    All Models set to admin site
    Every Models have been set to the admin.py to see it in the admin panel provided by DJANGO

--- backend/ProjectSeed/accounts/admin.py
+++ backend/ProjectSeed/accounts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from . models import User, Skill, Interest
 
-# Register your models here.
+
+# Registering the Models into the admin page
+admin.site.register(User)
+admin.site.register(Skill)
+admin.site.register(Interest)
