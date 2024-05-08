56f69ec3052f90c585977d7a6da4a830934c4cbe
Diwash Pandey <155344419+diwashpandey@users.noreply.github.com>
2024-05-08 11:53:25 +0545

    User Registration View - Developed
    The User Registration View has been implemented utilizing the CustomUserRegistration class.
    
    Note:
    Right now, it doesn't use serializers or anything extra. But we might add them later if needed.

--- backend/ProjectSeed/accounts/urls.py
+++ backend/ProjectSeed/accounts/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-       
+    path("register-user/", views.RegisterUser.as_view()),
 ]
--- backend/ProjectSeed/accounts/views.py
+++ backend/ProjectSeed/accounts/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+# Django built-in module to get the AUTH User
+from django.contrib.auth import get_user_model
+
+# Getting the user model using the django provided function
+User = get_user_model()
+
+# importing serializers
+# from . serializers import UserRegisterationSerializer
+
+# importing json for JSON works
+import json
+
+# Custom Class for creating user account
+from . registration import CustomUserRegistration
+
+class RegisterUser(APIView, CustomUserRegistration):
+
+    def post(self, request):
+        data = request.data
+
+        created_user = self.register_user_if_valid(registration_data=data)  # this returns user if created else None
+
+        return Response({
+                "success":self.success, # set by CustomUserRegistration automatically
+                "message":self.message_to_client # set by CustomUserRegistration automatically
+            })
