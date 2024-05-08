dc1e1cd85f2f5c47720a620c0d4bdfcaed6c0250
Diwash Pandey <155344419+diwashpandey@users.noreply.github.com>
2024-05-08 10:44:16 +0545

    Configured the CORS library
    configured the CORS library to prevent Cross-Origin Resource Sharing (CORS) errors in the frontend, which commonly occur when utilizing React

--- backend/ProjectSeed/ProjectSeed/settings.py
+++ backend/ProjectSeed/ProjectSeed/settings.py
@@ -40,8 +40,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'rest_framework',
+    #CORS header
+    "corsheaders",
 
+    # API framework
+    'rest_framework',
 
     # Custom made APPS
     'accounts',
@@ -56,6 +59,9 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+
+    # Added the cors headers to avoid the CORS error in the frontend(react)
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -146,3 +152,8 @@ MEDIA_ROOT = BASE_DIR / "media"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
