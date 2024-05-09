3508dfcc5cf541ce83f8ae25cc72ff10c911479e
Diwash Pandey <155344419+diwashpandey@users.noreply.github.com>
2024-05-09 15:56:14 +0545

    Component Header has been Developed
    Added Header along with the Single Page Linking support.
    
    The nav has the isActive theme support too.
    
    Nav icons has been put separately

---
+++ frontend/ProjectSeed/src/components/Header/Header.jsx
@@ -0,0 +1,59 @@
+import { Link, NavLink } from "react-router-dom"
+import HomeIcon from "./icons/Home"
+import TopPosts from "./icons/TopPosts"
+import TopProfiles from "./icons/TopProfiles"
+
+function Header(){
+  return (
+    <header className="h-[60px] w-full mb-2 flex justify-between items-center px-2 bg-main-box md:px-6 fixed z-10">
+        <div className="logo-and-search center gap-3">
+            <span>LOGO</span>
+            <form action="">
+                <input type="text" placeholder="Search for talents" className="hidden h-7 w-48 pl-4 border-[1px] border-orange-500 rounded-2xl bg-theme-darker text-xs md:block focus-visible:outline-none "/>
+            </form>
+        </div>
+
+        <nav className="h-full flex absolute bottom-0 left-[50%] translate-x-[-50%]">
+            <ul className="center gap-1 sm:gap-4 md:gap-8">
+                <li className="w-16 h-full">
+                    <NavLink to="/" className={(e) => {
+                        return ( e.isActive ? "nav-btn-active h-full w-16 p-4 center md:p-3" : "nav-btn h-full w-16 p-4 center md:p-3")
+                        }}>
+                    <HomeIcon />
+                    </NavLink>
+                </li>
+
+                <li className="w-16 h-full">
+                    <NavLink to="/login" className={(e) => {
+                            return ( e.isActive ? "nav-btn-active h-full w-16 p-4 center md:p-3" : "nav-btn h-full w-16 p-4 center md:p-3")
+                            }}>
+                        <TopPosts />
+                    </NavLink>
+                </li>
+
+                <li className="w-16 h-full">
+                    <NavLink to="/register" className={(e) => {
+                            return ( e.isActive ? "nav-btn-active h-full w-16 p-4 center md:p-3" : "nav-btn h-full w-16 p-4 center md:p-3")
+                            }}>
+                        <TopProfiles />
+                    </NavLink>
+                </li>
+
+            </ul>
+        </nav>
+
+            <div className="header-profile center gap-1">
+                <a href="" className="hidden md:block">
+                    <img src="./me.jpg" alt="" className="profile-photo h-10 w-10"/>
+                </a>
+                <div className="ellipsis-btn h-7 w-7 center flex-col cursor-pointer rounded-full p-1 hover:bg-slate-600">
+                    <div className="rounded-full h-1 w-1 m-auto bg-white"></div>
+                    <div className="rounded-full h-1 w-1 m-auto bg-white"></div>
+                    <div className="rounded-full h-1 w-1 m-auto bg-white"></div>
+                </div>
+            </div>
+    </header>
+  )
+}
+
+export default Header
