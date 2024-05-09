f5be578fbdba0af33f22d338af1c7905f704f373
Diwash Pandey <155344419+diwashpandey@users.noreply.github.com>
2024-05-09 15:56:55 +0545

    Added SeedCSS in the main.jsx
    SeedCss (Custom CSS library for "project seed") has been added to the main.jsx

--- frontend/ProjectSeed/src/main.jsx
+++ frontend/ProjectSeed/src/main.jsx
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import './seedcss/seedcss-base.css'
+import './seedcss/seedcss-buttons.css'
+import './seedcss/seedcss-components.css'
+import './seedcss/seedcss-utilities.css'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
