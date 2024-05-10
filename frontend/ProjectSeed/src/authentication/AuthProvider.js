d1816ad5b16963f2f06352cc48994955dce4f934
Diwash Pandey <155344419+diwashpandey@users.noreply.github.com>
2024-05-10 08:21:31 +0545

    Frontend: custom Auth Provider has been created
    AuthProvider has been created which provides the auth context like:
            isAuthenticated,
            setIsAuthenticated,
    
            authTokens,
            setAuthTokens
    
    I'll add more in future if required.

---
+++ frontend/ProjectSeed/src/authentication/AuthProvider.js
@@ -0,0 +1,43 @@
+import { useState, useEffect, createContext } from "react";
+
+
+/*
+    This manages things like:
+        1. isAuthenticated
+        2. setIsAuthenticated
+        
+        3. authTokens
+        4. setAuthTokens
+        
+*/
+
+const AuthContext = createContext()
+
+function AuthProvider({children}){
+
+    // isAuthenticated will be set to true if tokens exists in the local storage
+    const [isAuthenticated, setIsAuthenticated] = useState(()=>{
+        localStorage.getItem("authTokens") ? true : false
+    })
+
+    // setting the authTokens
+    const [authTokens, setAuthTokens] = useState(JSON.stringify(localStorage.getItem("authTokens")))
+
+    // These are the context that childrens can use
+    const contextData = {
+        isAuthenticated,
+        setIsAuthenticated,
+
+        authTokens,
+        setAuthTokens
+    }
+
+    // Wrapping it's chilren with it's values
+    return (
+        <AuthContext.Provider value={contextData}>
+            {children}                                      /* This is too much important concept, Don't forget !!! */
+        </AuthContext.Provider>
+    )
+}
+
+export default AuthProvider
