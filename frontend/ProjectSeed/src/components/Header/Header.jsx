cdeef792b7154bcc38be0bd993c248f5210b8a4a
Diwash Pandey <155344419+diwashpandey@users.noreply.github.com>
2024-05-10 13:53:17 +0545

    Frontend: Added conditional rendering in Header
    Conditional Rendering has been added to the right side of the Header.
    
    It will show login button if user is not authenticated.

--- frontend/ProjectSeed/src/components/Header/Header.jsx
+++ frontend/ProjectSeed/src/components/Header/Header.jsx
@@ -1,13 +1,25 @@
+// React imports
+import { useContext } from "react"
+
+// Third Parties Imports
 import { Link, NavLink } from "react-router-dom"
+
+// Pages and Components import
 import HomeIcon from "./icons/Home"
 import TopPosts from "./icons/TopPosts"
 import TopProfiles from "./icons/TopProfiles"
 
+// Additional Imports
+import { AuthContext } from "../../authentication/AuthProvider"
+
 function Header(){
+
+  const {isAuthenticated, setIsAuthenticated} = useContext(AuthContext)
+
   return (
     <header className="h-[60px] w-full mb-2 flex justify-between items-center px-2 bg-main-box md:px-6 fixed z-10">
         <div className="logo-and-search center gap-3">
-            <span>LOGO</span>
+            <span className="text-theme-color font-semibold">LOGO</span>
             <form action="">
                 <input type="text" placeholder="Search for talents" className="hidden h-7 w-48 pl-4 border-[1px] border-orange-500 rounded-2xl bg-theme-darker text-xs md:block focus-visible:outline-none "/>
             </form>
@@ -43,14 +55,25 @@ function Header(){
         </nav>
 
             <div className="header-profile center gap-1">
-                <a href="" className="hidden md:block">
-                    <img src="./me.jpg" alt="" className="profile-photo h-10 w-10"/>
-                </a>
-                <div className="ellipsis-btn h-7 w-7 center flex-col cursor-pointer rounded-full p-1 hover:bg-slate-600">
-                    <div className="rounded-full h-1 w-1 m-auto bg-white"></div>
-                    <div className="rounded-full h-1 w-1 m-auto bg-white"></div>
-                    <div className="rounded-full h-1 w-1 m-auto bg-white"></div>
-                </div>
+                {
+                    // Rendering this part according to the authentication
+                    isAuthenticated ? <>
+                        
+                        <a href="" className="hidden md:block">
+                            <img src="./me.jpg" alt="" className="profile-photo h-10 w-10"/>
+                        </a>
+                        <div className="ellipsis-btn h-7 w-7 center flex-col cursor-pointer rounded-full p-1 hover:bg-slate-600">
+                            <div className="rounded-full h-1 w-1 m-auto bg-white"></div>
+                            <div className="rounded-full h-1 w-1 m-auto bg-white"></div>
+                            <div className="rounded-full h-1 w-1 m-auto bg-white"></div>
+                        </div>
+                    </> :
+                    // Else
+                    <>
+                        <Link className="rise-btn" to="/login/">Login</Link>
+                    </>
+                }
+                
             </div>
     </header>
   )
