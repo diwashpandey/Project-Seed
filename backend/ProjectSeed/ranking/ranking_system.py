c7722fa86e15c13cb384d6e9b94d7fee212d9313
Diwash Pandey <155344419+diwashpandey@users.noreply.github.com>
2024-05-10 08:18:17 +0545

    name changes in ranking_system.py
    calling Top Students was sounding kindda irrelevant so changed to TopProfiles
    
    imports have been organized too.

--- backend/ProjectSeed/ranking/ranking_system.py
+++ backend/ProjectSeed/ranking/ranking_system.py
@@ -1,14 +1,16 @@
+# imports from djagno
+from django.db.models import F
+from django.contrib.auth import get_user_model
+from django.urls import reverse_lazy
+
 # Models import
 from colleges.models import College
 from universities.models import University
-from django.contrib.auth import get_user_model
-from django.db.models import F
 
-from django.urls import reverse_lazy
-
+# Getting the user model with django provided settings
 User = get_user_model()
 
-class RankSystem:
+class ProfileRankingSystem:
     """
         This is a class designed to fetch top students from either a college, university, or overall:
 
@@ -20,36 +22,39 @@ class RankSystem:
         Returns:
             data (dictionary) or None
 
-        This class automatically sets the message_for_client and success_or_not attributes.
-
+        This class has the message_to_client and success_status attributes too,
+        You can use them to send to the client
     """
 
-    success_or_not:bool = False
-    message_for_client:str = ""
+    success_status:bool = False
+    message_to_client:str = ""
 
-    def get_top_students_from_college(self, college_name, count=10, *args, **kwargs):
+    def get_top_profiles_from_college(self, college_name, count=10, *args, **kwargs):
+
         try:
             college = College.objects.get(name = college_name)
-            data = college.college_students.all().order_by("-rise_points")[0:count].annotate(profile_url = reverse_lazy("profile_page", kwargs={"username":F("username")})).values("name", "username", "rise_points")
-            self.success_or_not = True
+            data = college.college_students.all().order_by("-rise_points")[0:count].values("name", "username", "rise_points")
+            self.success_status = True
             return data
+
         except:
-            self.message_for_client = "College doesn't exists"
+            self.message_to_client = "College doesn't exists"
             return None
 
-    def get_top_students_from_university(self, university_name, count=10, *args, **kwargs):
+    def get_top_profiles_from_university(self, university_name, count=10, *args, **kwargs):
+
         try:
             university = University.objects.get(name = university_name)
-            data = university.university_students.all().order_by("-rise_points")[0:count].annotate(profile_url = reverse_lazy("profile_page", kwargs={"username":F()})).values("name", "username", "rise_points")
-            self.success_or_not = True
-            
+            data = university.university_students.all().order_by("-rise_points")[0:count].values("name", "username", "rise_points")
+            self.success_status = True
             return data
+
         except:
-            self.message_for_client = "This university doesn't exists"
+            self.message_to_client = "This university doesn't exists"
             return None
     
-    def get_top_students_from_global(self, count=10, *args, **kwargs):
-        data = list(User.objects.all().order_by("-rise_points")[0:count].annotate(profile_url = reverse_lazy("profile_page", kwargs={"username":F()})).values("full_name", "username", "rise_points"))
-        self.success_or_not = True
+    def get_top_profiles_from_global(self, count=10, *args, **kwargs):
 
+        data = list(User.objects.all().order_by("-rise_points")[0:count].values("full_name", "username", "rise_points"))
+        self.success_status = True
         return data
