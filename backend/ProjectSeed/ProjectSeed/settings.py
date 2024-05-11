49fdccabb6cb8a675ebe1fd5cf4842a03b5b43b8
Diwash Pandey <155344419+diwashpandey@users.noreply.github.com>
2024-05-11 22:45:09 +0545

    Added CORS settings in the settings
    CORS settings has been added to let the FRONTEND be able to get connect to the API

--- backend/ProjectSeed/ProjectSeed/settings.py
+++ backend/ProjectSeed/ProjectSeed/settings.py
@@ -38,8 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    # 'django.contrib.messages', Commented out this cause we don't need this
+    'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    "corsheaders",
 
     #rest_framwork APPS
     'rest_framework',
@@ -58,6 +60,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -152,6 +155,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 #
 # Third party libraries settings 
 #
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",  # Add your frontend URL here
+    "http://127.0.0.1:5173",   # Add your frontend URL here
+    # Add more origins if needed
+]
+
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
@@ -160,7 +169,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=15),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "ROTATE_REFRESH_TOKENS": False,
     "BLACKLIST_AFTER_ROTATION": False,
