04d0e198e5f965a074e6fd8d6d3aa9c5486aa9fc
Diwash Pandey <155344419+diwashpandey@users.noreply.github.com>
2024-05-11 22:44:02 +0545

    Added Token Refresh API View
    Token Refresh point has been added which is default provided by the simple_jwt library.

--- backend/ProjectSeed/accounts/urls.py
+++ backend/ProjectSeed/accounts/urls.py
@@ -1,7 +1,14 @@
+# imports from djagno
 from django.urls import path
+
+# imports from rest_framework_simplejwt
+from rest_framework_simplejwt.views import TokenRefreshView
+
+# imports from APPS
 from . import views
 
 urlpatterns = [
        path("login/", views.LoginView.as_view(), name="login"),
        path("register/", views.RegisterView.as_view(), name="register"),
+       path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
