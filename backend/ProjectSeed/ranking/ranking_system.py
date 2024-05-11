3bc0ad4aa6fbd737a07656fff42ad184775fd8cc
Diwash Pandey <155344419+diwashpandey@users.noreply.github.com>
2024-05-11 22:40:15 +0545

    Added Posts Ranking System
    Posts Ranking System has been developed in the ranking_system.py.
    It's similar to Profile Ranking system.

--- backend/ProjectSeed/ranking/ranking_system.py
+++ backend/ProjectSeed/ranking/ranking_system.py
@@ -6,6 +6,7 @@ from django.urls import reverse_lazy
 # Models import
 from colleges.models import College
 from universities.models import University
+from posts.models import Post
 
 # Getting the user model with django provided settings
 User = get_user_model()
@@ -33,7 +34,6 @@ class ProfileRankingSystem:
 
         try:
             college = College.objects.get(name = college_name)
-            data = college.college_students.all().order_by("-rise_points")[0:count].values("name", "username", "rise_points")
             data = college.college_students.all().order_by("-rise_points")[0:count].values("full_name", "username", "rise_points")
             self.success_status = True
             return data
@@ -46,7 +46,6 @@ class ProfileRankingSystem:
 
         try:
             university = University.objects.get(name = university_name)
-            data = university.university_students.all().order_by("-rise_points")[0:count].values("name", "username", "rise_points")
             data = university.university_students.all().order_by("-rise_points")[0:count].values("full_name", "username", "rise_points")
             self.success_status = True
             return data
@@ -58,5 +57,56 @@ class ProfileRankingSystem:
     def get_top_profiles_from_global(self, count=10, *args, **kwargs):
 
         data = list(User.objects.all().order_by("-rise_points")[0:count].values("full_name", "username", "rise_points"))
+        self.success_status = True
+        return data
+
+
+class PostsRankingSystem:
+    """
+        This is a class designed to fetch top students from either a college, university, or overall:
+
+        Methods:
+            get_top_students_from_college (college_name, count)
+            get_top_students_from_university (university_name, count)
+            get_top_students_from_global (count)
+
+        Returns:
+            data (dictionary) or None
+
+        This class has the message_to_client and success_status attributes too,
+        You can use them to send to the client
+    """
+
+    success_status:bool = False
+    message_to_client:str = ""
+
+    def get_top_posts_from_college(self, college_name, count=10, *args, **kwargs):
+
+        try:
+            college = College.objects.get(name = college_name)
+            data = Post.objects.filter(user__colleges = college).order_by("-rises_count")[0:count]
+            self.success_status = True
+            return data
+
+        except:
+            self.message_to_client = "College doesn't exists"
+            return None
+
+    def get_top_posts_from_university(self, university_name, count=10, *args, **kwargs):
+
+        try:
+            university = University.objects.get(name = university_name)
+            data = Post.objects.filter(user__universities = university).order_by("-rises_count")[0:count]
+            self.success_status = True
+            return data
+
+        except:
+            self.message_to_client = "This university doesn't exists"
+            return None
+    
+    def get_top_posts_from_global(self, count=10, *args, **kwargs):
+
+        data = list(Post.objects.all().order_by("-rises_count")[0:count])
+        print("data")
         self.success_status = True
         return data
