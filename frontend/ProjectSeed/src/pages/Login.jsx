bf8c177326be419dc185e2e9b13f5f7264cc7118
Diwash Pandey <155344419+diwashpandey@users.noreply.github.com>
2024-05-12 10:01:59 +0545

    Login page has been added
    - Login.jsx has been added which has the html
    - Uses the loginHandler to handle the login request from the user.

--- frontend/ProjectSeed/src/pages/Login.jsx
+++ frontend/ProjectSeed/src/pages/Login.jsx
@@ -1,6 +1,16 @@
-import { useState } from "react";
+// React imports
+import { useEffect, useState } from "react";
 
+// Third partied libraries import
+import { Link, redirect } from "react-router-dom";
+
+// Components and Pages imports
 import Header from "../components/Header/Header"
+
+// Additional imports
+import LoginHandler from "../authentication/loginHandler"
+
+const loginHandler = new LoginHandler()
 
 function Login() {
 
@@ -8,6 +18,12 @@ function Login() {
     "email":"",
     "password":""
   })
+
+  let [messageFromServer, setMessageFromServer] = useState(null)
+  let [messageBoxColor, setMessageBoxColor] = useState("bg-[#a91414]")
+
+  useEffect(()=>{
+  }, [messageFromServer])
 
   const handleChange = (e) =>{
     setFormData({
@@ -17,35 +33,57 @@ function Login() {
   }
 
   const handleSubmit = async(e) => {
-    
+    e.preventDefault()
+
+    const response = await loginHandler.handleLoginRequest(formData.email, formData.password);
+
+    if (response.successStatus === true){
+      setMessageFromServer(response.messageFromServer);
+      setMessageBoxColor("bg-green-600");
+      setTimeout(()=>{
+        window.location.href = "/";
+      }, 1000)
+    }
+    else{
+      setMessageFromServer(response.messageFromServer);
+      setMessageBoxColor("bg-[#a91414]");
+    }
   }
 
   return (
     <>
     <Header />
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <div className="max-w-md w-full p-8 login-container">
-        <h2 className="text-3xl font-semibold text-center mb-6 text-gray-800">Login</h2>
-        <form>
-          <div className="mb-6">
-            <label htmlFor="email" className="block text-gray-700 text-sm font-bold mb-2">Email Address</label>
-            <input type="email" id="email" name="email" value = {formData.email} onChange={handleChange}
-              className="border-gray-300 border text-black rounded-md px-4 py-2 w-full focus:outline-none focus:border-blue-400"
-              placeholder="Enter your email address" required />
-          </div>
-          <div className="mb-6">
-            <label htmlFor="password" className="block text-gray-700 text-sm font-bold mb-2">Password</label>
-            <input type="password" id="password" name="password" value = {formData.password} onChange={handleChange}
-              className="border-gray-300 border text-black rounded-md px-4 py-2 w-full focus:outline-none focus:border-blue-400"
-              placeholder="Enter your password" required />
-          </div>
-          <button type="submit" className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-md w-full">Login</button>
-        </form>
-        <p className="mt-4 text-sm text-gray-600 text-center">Don't have an account? <a href="#"
-          className="text-blue-500 hover:underline">Sign up</a></p>
-      </div>
-    </div>
-          </>
+    <main className="h-screen w-screen center">
+      <form id="login-box" className="h-[30rem] w-[26rem] p-8 rounded-lg flex flex-col items-center relative bg-main-box"  onSubmit={handleSubmit}>
+
+        {/* Showing the messgae from the server when get */}
+        {
+        messageFromServer != null ? (
+              <p id="message-from-server" className={`p-2 max-w-full absolute rounded-lg top-[-10%] ${messageBoxColor}`}>
+                {messageFromServer}
+              </p>
+            ) : null
+          }
+        
+        <h1 className="mb-16 text-3xl text-center text-theme-color">Login to Seed</h1>
+        <div className="mb-6">
+          <p className="text-sm">Email</p>
+          <input type="email" className="input-box h-8 w-64 max-w-full my-2" name="email" value={formData.email} onChange={handleChange} required/>
+        </div>
+        <div className="mb-3">
+          <p className="text-sm">Password</p>
+          <input type="password" className="input-box h-8 w-64 max-w-full my-2" name="password" value={formData.password} onChange={handleChange} required/>
+          <p className="text-xs text-end">Forgot Password?</p>
+        </div>
+        <button type="submit" className="hollow-theme-btn mb-4" > Login </button>
+        <div id="seperators" className="flex w-full justify-between mb-4">
+          <div id="line1" className="w-[40%] h-0.5 bg-theme-lighter"></div>
+          <div id="line2" className="w-[40%] h-0.5 bg-theme-lighter"></div>
+        </div>
+        <p className="text-sm">Don't have an account? <Link className="text-theme-color hover:underline">Create a new account</Link></p>
+      </form>
+    </main>
+    </>
   );
 }
 
