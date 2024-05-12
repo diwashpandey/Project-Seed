c67f7e926317b9c513001eaff54b5f7ad7272479
Diwash Pandey <155344419+diwashpandey@users.noreply.github.com>
2024-05-12 10:00:11 +0545

    More work in HomePageView
    Little bugs has been fixed for eg:
    request.user.colleges.all()[0] : added all()
    
    Added user profile data too in the home page.

--- backend/ProjectSeed/home/views.py
+++ backend/ProjectSeed/home/views.py
@@ -2,6 +2,8 @@
 from rest_framework.generics import ListAPIView
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
+
 
 # imports from apps
 from ranking.ranking_system import ProfileRankingSystem, PostsRankingSystem
@@ -9,36 +11,41 @@ from utilities.response.response_utilities import ResponseUtilities
 
 # imports from django
 from django.http import HttpResponse
+from django.contrib.auth import get_user_model
 
 # importing serializers
 from ranking.serializers import TopProfilesSerializer
 from posts.serializers import PostSerializer
 from accounts.serializers import UserProfileSerializer
 
+# using djagno provided function to get the USER Model
+User = get_user_model()
+
 # Create your views here.
+def temporaryview(request):
+    return HttpResponse("I am in home")
+
 class HomePageView(APIView, ResponseUtilities, ProfileRankingSystem, PostsRankingSystem):
+    authentication_classes = []  # Empty list means no authentication required
+    permission_classes = [AllowAny]  # Allow any user to access
     
     def get(self, request, format=None):
-
-        #           *******   Fetching Top Profiles *******
-
+        
         top_profiles = None  # This will be sent to the Client 
         top_posts = None  # This will be sent to the Client
-        user = None
+        loggedin_user_details = None
         
         if request.user.is_authenticated:
-            print("Authenticated user request in home view")
-
-            user = request.user
-            user = UserProfileSerializer(instance = user)
+            
+            loggedin_user_details = UserProfileSerializer(instance = request.user).data
 
             if request.user.colleges:
-                top_profiles = self.get_top_profile_from_college(request.user.colleges[0].name, count = 3)
-                top_posts = self.get_top_posts_from_college(request.user.colleges[0].name, count = 3)
+                top_profiles = self.get_top_profiles_from_college(request.user.colleges.all()[0].name, count = 3)
+                top_posts = self.get_top_posts_from_college(request.user.colleges.all()[0].name, count = 3)
 
             elif request.user.universities:
-                top_profiles = self.get_top_profiles_from_university(request.user.universities[0].name, count = 3)
-                top_posts = self.get_top_posts_from_university(request.user.universities[0].name, count = 3)
+                top_profiles = self.get_top_profiles_from_university(request.user.universities.all()[0].name, count = 3)
+                top_posts = self.get_top_posts_from_university(request.user.universities.all()[0].name, count = 3)
             else:
                 top_profiles = self.get_top_profiles_from_global(count = 3)
                 top_posts = self.get_top_posts_from_global(count = 3)
@@ -52,11 +59,9 @@ class HomePageView(APIView, ResponseUtilities, ProfileRankingSystem, PostsRankin
         top_profiles_serialized_data = TopProfilesSerializer(instance = top_profiles, many=True)
         top_posts_serialized_data = PostSerializer(instance = top_posts, many=True)
 
-        # print("The top profiles are:", top_profiles_serialized_data) # This is temporary code for testing(Remove if you see it)
-
         # Adding the data to the response_data
-        self.response_data = {"top3Photos":top_profiles_serialized_data.data,
-                              "top3Posts": top_posts_serialized_data.data,
-                              "user":user}
+        self.response_data = {"top_3_profiles":top_profiles_serialized_data.data,
+                              "top_3_posts": top_posts_serialized_data.data,
+                              "loggedin_user_details":loggedin_user_details}
 
         return Response(self.get_generated_response())
