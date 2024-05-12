76f2c06abf3cfd3badf289857504edc660cb517c
Diwash Pandey <155344419+diwashpandey@users.noreply.github.com>
2024-05-12 09:57:58 +0545

    useAxios Hook made more flexible
    Now useAxios can be used for non authenticated request too.
    
    For eg:
    Home page can be accessed by non authenticated users too.

--- frontend/ProjectSeed/src/hooks/useAxios.js
+++ frontend/ProjectSeed/src/hooks/useAxios.js
@@ -7,68 +7,71 @@ import axios from "axios";
 
 // Imports from src
 import { AuthContext } from "../authentication/AuthProvider";
-import { jwtDecode } from "jwt-decode";
 
-const BASEURL = "http://127.0.0.1:8000/"
-const TokenRefreshURl = `${BASEURL}/authentication/token/refresh`
+const BASEURL = "http://127.0.0.1:8000"
+const TokenRefreshURl = `${BASEURL}/account/token/refresh/`
 
 function useAxios(){
 
-    const {authTokens, setAuthTokens} = useContext(AuthContext)
-
-    /*
-        Customizing the axios, Attatching the BASEURL and Headers
-    */
-    const customAxios = axios.create({
-        baseURL: BASEURL,
-        timeout: 6000,                                        // Now These will go with every request
-        headers: {'Authorization': `Bearer ${authTokens}`} 
-      });
-
-
-    /*
-      Overriding the axios request.
-      Checking the tokens expiry before sending the axios request
-    */
-    customAxios.interceptors.request.use( async (request) => {
-      
-      const accessToken = jwtDecode(authTokens.access);  // decoded access token
-      const isExpired = dayjs.unix(accessToken.exp).diff(dayjs()) < 1; // checking if exipred
-
-      if ( !isExpired ){ // is token is not expired
-        return request   // Returning the request as it is
-      }
-
-      /*
-        
-        Now renewing the access token cause it's expired
-
-      */
-
-      // Sending the REFRESH request to the Backend
-      const response = await axios.post(TokenRefreshURl, {
-        refresh: authTokens.refresh
-      });
-
-      // Setting the authTokens to the local storage
-      localStorage.setItem("authTokens", JSON.stringify(response.data));
-
-      // Changing the authTokens state
-      setAuthTokens = JSON.stringify(response.data)
-
-      /*
-        Rewriting the header of the axios and putting new token
-      */
-      req.headers.Authorization = `Bearer ${response.data.access}`;
-
-      return request; // returning the renewed request
-    } )
+    const {authTokens, setAuthTokens, isAuthenticated, setIsAuthenticated} = useContext(AuthContext)
+    console.log("here 1")
     
-
-    /* 
-      Finally ! Returning the custom made Axios
+    /*
+    Customizing the axios, Attatching the BASEURL and Headers
     */
-      return customAxios
+   const customAxios = axios.create({
+     baseURL: BASEURL,
+     timeout: 6000,                                        // Now These will go with every request
+     headers: {'Authorization': `Bearer ${authTokens}`} 
+    });
+    
+    
+    /*
+    Overriding the axios request if user is Authenticated.
+    Checking the tokens expiry before sending the axios request
+    */
+    if (isAuthenticated){
+
+        customAxios.interceptors.request.use(async (request) => {
+            const accessToken = jwtDecode(authTokens.access);  // decoded access token
+            console.log("here 2")
+            const isExpired = Date.now() >= accessToken.exp // checking if exipred
+            if ( !isExpired ){ // is token is not expired
+                return request   // Returning the request as it is
+              }
+              
+              /*
+                
+                Now renewing the access token cause it's expired
+
+                */
+              // Sending the REFRESH request to the Backend
+              const response = await axios.post(TokenRefreshURl, {
+                refresh: authTokens.refresh
+              });
+
+              // Setting the authTokens to the local storage
+              localStorage.setItem("access", response.data.access) // Disclaimer! Don't forget to put the reqeust.data.access ! Other wise it will go as object ! it fucking took me 1 day to debug this shit lol
+
+              // Changing the authTokens state
+              setAuthTokens({
+                "refresh":authTokens.refresh,
+                "access":response.access
+              })
+
+              /*
+                Rewriting the header of the axios and putting new token
+              */
+              request.headers.Authorization = `Bearer ${response.data.access}`;
+              return request; // returning the renewed request
+          } )
+          
+
+          /* 
+            Finally ! Returning the custom made Axios
+          */
+        }
+    return customAxios
 }
 
 export default useAxios
