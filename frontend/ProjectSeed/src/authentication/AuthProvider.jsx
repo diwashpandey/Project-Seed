48e71216fa8b3817e325c65893c79045d70de2d5
Diwash Pandey <155344419+diwashpandey@users.noreply.github.com>
2024-05-12 09:54:58 +0545

    Frontend: Added More Features in AuthProvider
    In AuthProvider, added functions like tokenExists and checkIfRefreshTokenIsValid.
    
    Both provides more advancement and features in the AuthProvider.
    
    # We'll work more here in the future

--- frontend/ProjectSeed/src/authentication/AuthProvider.jsx
+++ frontend/ProjectSeed/src/authentication/AuthProvider.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect, createContext } from "react";
+import { jwtDecode } from "jwt-decode";
 
 
 /*
@@ -13,16 +14,55 @@ import { useState, useEffect, createContext } from "react";
 
 export const AuthContext = createContext()
 
+function tokensExists(){
+    const refresh = localStorage.getItem("refresh")
+    const access = localStorage.getItem("access")
+
+    return (refresh !== null && access !== null) // Will be FALSE if not null else TRUE
+}
+
+function checkIfRefreshTokenIsValid(){
+    try{
+        
+        // Getting the refresh token
+        const refresh = jwtDecode(localStorage.getItem("refresh"))
+        
+        // Checking if the refresh token expiry date
+        if(Date.now() >= refresh.exp){
+            return true
+        }
+        else {
+            // // Removing the JSON Web Tokens if expired
+            localStorage.removeItem("refresh")
+            localStorage.removeItem("access")
+            return false
+        }
+    }
+    catch(error){
+        // Removing the JSON Web Tokens if some glitch happened while decoding them
+        localStorage.removeItem("refresh")
+        localStorage.removeItem("access")
+        return false
+    }
+}
+
 function AuthProvider({children}){
+
+    if (tokensExists()){
+        checkIfRefreshTokenIsValid()
+    }
 
     // isAuthenticated will be set to true if tokens exists in the local storage
     const [isAuthenticated, setIsAuthenticated] = useState(()=>{
-        localStorage.getItem("authTokens") ? true : false
+        return localStorage.getItem("refresh") ? true : false
     })
 
     // setting the authTokens
-    const [authTokens, setAuthTokens] = useState(JSON.stringify(localStorage.getItem("authTokens")))
-
+    const [authTokens, setAuthTokens] = useState({
+        "access":localStorage.getItem("access"),
+        "refresh":localStorage.getItem("refresh")
+    })
+    
     // These are the context that childrens can use
     const contextData = {
         isAuthenticated,
@@ -35,7 +75,7 @@ function AuthProvider({children}){
     // Wrapping it's chilren with it's values
     return (
         <AuthContext.Provider value={contextData}>
-            {children}                                      /* This is too much important concept, Don't forget !!! */
+            {children}                                      {/* This is too much important concept, Don't forget !!! */}
         </AuthContext.Provider>
     )
 }
