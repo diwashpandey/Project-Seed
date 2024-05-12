12e0cd434de571c4ff91f36c8c6e198a8ed2f552
Diwash Pandey <155344419+diwashpandey@users.noreply.github.com>
2024-05-12 09:58:51 +0545

    Bug fix in user profile serializer

--- backend/ProjectSeed/accounts/serializers.py
+++ backend/ProjectSeed/accounts/serializers.py
@@ -11,8 +11,7 @@ class UserProfileSerializer(serializers.ModelSerializer):
     
     class Meta:
         model = User
-        fields = "__all__"
-        exclude = ["password", "created_date", "first_name", "last_name"]
+        fields = ["email","username","full_name","age","gender","intro","about_me","profile_photo","background_photo","is_teacher","is_verified","is_email_verified","following_count","followers_count","rise_points","github","linkedin","instagram","colleges","universities","skills","interests","following", "rise"]
         
 
 
