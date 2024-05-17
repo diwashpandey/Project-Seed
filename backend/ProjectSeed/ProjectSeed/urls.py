2d89abbe6f646cfb3dad6c1efc34e3f93d616834
Diwash Pandey <155344419+diwashpandey@users.noreply.github.com>
2024-05-17 20:01:51 +0545

    Backend: urls Cleanup

--- backend/ProjectSeed/ProjectSeed/urls.py
+++ backend/ProjectSeed/ProjectSeed/urls.py
@@ -1,12 +1,18 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("home.urls")),
-    path('account/', include("accounts.urls")),
-    path('post/', include("posts.urls")),
-    path('college/', include("colleges.urls")),
-    path('university/', include("universities.urls")),
+    path('home/', include("home.urls")),
+    path('accounts/', include("accounts.urls")),
+    path('posts/', include("posts.urls")),
+    path('colleges/', include("colleges.urls")),
+    path('universities/', include("universities.urls")),
     path('ranking/', include("ranking.urls")),
+    path('recommendation/', include("recommendation.urls"))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
