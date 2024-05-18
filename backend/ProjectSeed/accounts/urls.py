e098eb1adb9eb5b206dbfae8560bfcb84837d420
Diwash Pandey <155344419+diwashpandey@users.noreply.github.com>
2024-05-18 21:44:38 +0545

    Forgot to add the url before

--- backend/ProjectSeed/accounts/urls.py
+++ backend/ProjectSeed/accounts/urls.py
@@ -12,6 +12,10 @@ urlpatterns = [
        path("login/", views.LoginView.as_view(), name="login"),
        path("register/", views.RegisterView.as_view(), name="register"),
 
+       # Profile
+       path("profile", views.ProfileView.as_view(), name="profile"),
+       path("profile-non-authenticated", views.ProfileViewNonAuthenticated.as_view(), name="profile"),
+
        # Tokens
        path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
