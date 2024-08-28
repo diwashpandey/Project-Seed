cfaecfb029cd8c2ef2f8a73e54eda37c69868f48
Diwash Pandey <155344419+diwashpandey@users.noreply.github.com>
2024-08-28 21:38:39 +0545

    Developed Fetcher for Signup Request
    Implemented a fetcher to handle the Signup request, sending user data to the server for account creation.

---
+++ frontend/ProjectSeed/src/fetchers/Register/fetchSignUpRequest.js
@@ -0,0 +1,32 @@
+import axios from "axios";
+import { SignUpURL } from "../../utilities/apiEndpoints";
+
+async function fetchSignUpRequest(formData) {
+    const data = purifyDataForBackend(formData)
+    try {
+        const response = await axios.post(SignUpURL, data);
+        if (response.data.success_status === true) {
+            return response.data.response_data;
+        } else {
+            return Promise.reject(response.data.message_from_server);
+        }
+    } catch (err) {
+        console.error(err); // Log the actual error for debugging
+        return Promise.reject("Something went wrong while uploading your post");
+    }
+}
+
+// This will change the key names as required for BackEnd
+function purifyDataForBackend(formData){
+    return{
+        "first_name":formData.firstName,
+        "last_name":formData.lastName,
+        "new_email":formData.email,
+        "new_username":formData.username,
+        "gender":formData.gender,
+        "new_password":formData.newPassword,
+        "confirm_password":formData.confirmPassword,
+    }
+}
+
+export default fetchSignUpRequest;
