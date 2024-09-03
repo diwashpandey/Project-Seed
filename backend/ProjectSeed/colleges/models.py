687a23455a1368708e504452c6c289b686a69a03
Diwash Pandey <155344419+diwashpandey@users.noreply.github.com>
2024-09-03 20:33:38 +0545

    Expanded College Model
    - Added new data fields to the College model.
    - Enhanced model functionality and performance.

--- backend/ProjectSeed/colleges/models.py
+++ backend/ProjectSeed/colleges/models.py
@@ -1,46 +1,57 @@
+# Imports from django
 from django.db import models
-from universities.models import University
 from django.utils.text import slugify
-
 from django.contrib.auth import get_user_model # Djagno given function to get the AUTH USER model
+
+# Models import
+from universities.models import University
 
 # Getting the User from the dajngo given settings
 User = get_user_model()
 
 class College(models.Model):
 
-    # Name
+    # Details
     name = models.CharField(max_length=256)
     college_identifier = models.SlugField(max_length=255, unique=True, blank=True)
+    intro = models.CharField(max_length = 100, null=True, blank=True)
+    about_us= models.TextField(max_length=1000, null=True, blank=True)
 
     # Profile Photos
     profile_photo = models.ImageField(default="college_default_profile_photo", upload_to="colleges_profile_photos/", blank=True, null=True)
     background_photo = models.ImageField(default="college_default_background_photo.jpg", upload_to = "colleges_background_photos/", blank=True, null=True)
 
-    #   Location and Country
+    # Location and Country
     location = models.CharField(max_length=200)
     country = models.CharField(max_length=128)
 
-    #   Verifications
+    # Verifications
     is_verified = models.BooleanField(default=False)
 
-    #           **********************     Relations     **********************     #
+    # Counts and Points
+    rise_points = models.IntegerField(default = 0, null=True)
+    
+    # Timestamps
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    # ********************** Relations ********************** #
 
     # Users who controls the page
-    host = models.ForeignKey(User, on_delete=models.SET_NULL, related_name="hosted_colleges", null=True, blank=True)
-    admins = models.ManyToManyField(User, blank=True, related_name="administered_colleges")
+    admin = models.ForeignKey(User, on_delete=models.SET_NULL, related_name="administered_colleges", null=True, blank=True)
+    management_team = models.ManyToManyField(User, blank=True, related_name="managed_colleges")
 
     # Foreign key to the University
     university = models.ForeignKey(University, related_name="colleges", on_delete=models.SET_NULL, null=True, blank=True)
 
-    #           ******************     End of Relations     ******************     #
+    # ****************** End of Relations ****************** #
 
     class Meta:
         ordering = ["name"]
 
     def save(self, *args, **kwargs):
-        if not self.slug:
-            self.slug = slugify(self.name)
+        if not self.college_identifier:
+            self.college_identifier = slugify(self.name)
         super().save(*args, **kwargs)
     
     def __str__(self) -> str:
