eb3b29d4bb7eaabc3a3931aba8e7dc284201593b
Diwash Pandey <155344419+diwashpandey@users.noreply.github.com>
2024-09-10 07:13:27 +0545

    Updated Points Counter for Model Changes
    Adjusted points counter logic to match recent model updates.
    Ensured accurate point calculations based on new model fields and relationships.

--- backend/ProjectSeed/ranking/points_counter.py
+++ backend/ProjectSeed/ranking/points_counter.py
@@ -1,5 +1,5 @@
 from django.db.models.functions import Coalesce
-from django.db.models import Count
+from django.db.models import Count, Sum
 from django.db.models import Q
 
 def get_risepoints_of_user(user):
@@ -14,15 +14,17 @@ def get_risepoints_of_user(user):
     """
 
     # Using 👉Coalesce👈 for if value is null 👇
-    # Getting the count of rises by teacher and friends
+    # Getting the count of rises according to the user's roles
     rises = user.risen_by.aggregate(
-            teachers_count= Coalesce(Count("id", filter=Q(is_teacher=True)), 0),friends_count=Coalesce(Count("id", filter=Q(is_teacher=False)),0)
+            teachers_count= Coalesce(Count("id", filter=Q(profession="t")), 0),
+            students_count=Coalesce(Count("id", filter=Q(profession="s")),0),
+            professionals_count=Coalesce(Count("id", filter=Q(profession="p")),0),
+            investors_count=Coalesce(Count("id", filter=Q(profession="i")),0)
             )
-        
     # Getting the rise for every posts that followed_user has created
     post_rises = user.posts.aggregate(rises_in_post = Coalesce(Count("rises__id"), 0))
 
     # Calculating points as Rise points calculation method
-    points = (rises["teachers_count"] * 5) + (rises["friends_count"] * 2) + post_rises["rises_in_post"]
-    print("got the point", points, "of user", user)
+    points = ((rises["teachers_count"] + rises["professionals_count"] + rises["investors_count"] ) * 5) + (rises["students_count"] * 2) + post_rises["rises_in_post"]
+    print(f"Got new RisePoints for '{user.full_name}':'{points}'")
     return points
