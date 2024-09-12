3609ad6925df207f198a7ea18ddc3ecb17d56914
Diwash Pandey <155344419+diwashpandey@users.noreply.github.com>
2024-09-12 21:37:26 +0545

    Update University Model
    Made changes to the University model to align with new requirements or improvements.
    Adjusted fields and relationships as needed.
    Updated related database schema and migration files.

--- backend/ProjectSeed/universities/models.py
+++ backend/ProjectSeed/universities/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.utils.text import slugify
 from django.contrib.auth import get_user_model # Djagno given function to get the AUTH USER model
 
 # Getting the User from the dajngo given settings
@@ -8,29 +8,40 @@ User = get_user_model()
 class University(models.Model):
 
     # Name
-    name = models.CharField(max_length=256)
+    name = models.CharField(max_length=80)
+    university_identifier = models.SlugField(max_length=255, unique=True, blank=True)
+    slogan = models.CharField(max_length = 100, null=True, blank=True)
+    about_us= models.TextField(max_length=1000, null=True, blank=True)
 
     # Profile Photos
     profile_photo = models.ImageField(default="university_default_profile_photo", upload_to="universities_profile_photos/", blank=True, null=True)
     background_photo = models.ImageField(default="university_default_background_photo.jpg", upload_to = "universities_background_photos/", blank=True, null=True)
 
     #   Location and Country
-    location = models.CharField(max_length=200)
-    country = models.CharField(max_length=128)
+    location = models.CharField(max_length=50)
+    country = models.CharField(max_length=15)
 
     #   Verifications
     is_verified = models.BooleanField(default=False)
 
+    # Timestamps
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
     #           **********************     Relations     **********************     #
 
     # Users who controls the page
-    host = models.ForeignKey(User, on_delete=models.SET_NULL, related_name="hosted_universities", null=True, blank=True)
-    admins = models.ManyToManyField(User, blank=True, related_name="administered_universities")
+    admin = models.ForeignKey(User, on_delete=models.SET_NULL, related_name="administered_universities", null=True, blank=True)
 
     #           ******************     End of Relations     ******************     #
 
     class Meta:
         ordering = ["name"]
 
+    def save(self, *args, **kwargs):
+        if not self.college_identifier:
+            self.college_identifier = slugify(self.name)
+        super().save(*args, **kwargs)
+    
     def __str__(self) -> str:
         return self.name
